8e271128b66a955e50853fb40ceb6bb2971a1e83
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2021-12-15 14:19:51 +0300

    Configured Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: ['./public/**/*.html', './src/**/*.vue'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {}
   },
