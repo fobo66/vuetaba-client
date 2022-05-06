cf698ad1a54be18f213ad140a52cef1fe2c2a064
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-05-07 02:01:20 +0300

    Reset development of the project from scratch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import './assets/tailwind.css'
+import './index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  purge: ['./public/**/*.html', './src/**/*.vue'],
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  variants: {
-    extend: {}
-  },
-  plugins: []
+  plugins: [],
 }
