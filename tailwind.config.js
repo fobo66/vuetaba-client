a2d08a30c6ce622c6ca958d1df3122c6cc3f3be3
Andrei Mukamolau <4471821+fobo66@users.noreply.github.com>
2023-12-19 12:16:02 +0000

    Improved Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
